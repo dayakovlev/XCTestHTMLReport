834c88e6d71bfa55703822ca594f5383ecc1d85d
dayakovlev <59823322+dayakovlev@users.noreply.github.com>
2020-01-15 18:27:52 +0300

    change branch for formula

--- xchtmlreport.rb
+++ xchtmlreport.rb
@@ -3,7 +3,7 @@ class Xchtmlreport < Formula
   homepage "https://github.com/TitouanVanBelle/XCTestHTMLReport"
   url "https://github.com/TitouanVanBelle/XCTestHTMLReport/archive/2.0.0.tar.gz"
   sha256 "4424d673d578e84e67fd96afa53a5bd3e80ec7acade65365a123af358c77b47e"
-  head "https://github.com/TitouanVanBelle/XCTestHTMLReport.git", :branch => "develop"
+  head "https://github.com/dayakovlev/XCTestHTMLReport.git", :branch => "fix"
 
   def install
     system "swift build --disable-sandbox -c release"
